eaae538fe5ec4968d4ca11725ede26fc705977a7
AdrianPerez221 <aperez@student.ifp.es>
2025-05-07 10:07:41 +0200

    d

--- podekex para conectar/php/db.php
+++ podekex para conectar/php/db.php
@@ -3,7 +3,7 @@ $host = "localhost";
 $user = "";
 $password = "";
 $database = "";
-
+// usuario en la base de datos
 $conn = new mysql($host, $user, $password, $database);
 
 if($conn->connect_error){
